0c80a4cdfc8782142ebe1e9a98ffa8d7de8f212e
cemperlol <Baxeon96@yandex.ru>
2026-01-16 15:19:25 +0300

    documented all enums

--- src/main/java/com/travel/hero/attachment/enumeration/AttachmentType.java
+++ src/main/java/com/travel/hero/attachment/enumeration/AttachmentType.java
@@ -1,9 +1,31 @@
 package com.travel.hero.attachment.enumeration;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Attachment type")
 public enum AttachmentType {
 
+    @Schema(
+            description = "Image attachment.",
+            example = "png, jpg, etc."
+    )
     IMAGE,
+
+    @Schema(
+            description = "Video attachment.",
+            example = "mp4, mov, etc."
+    )
     VIDEO,
+
+    @Schema(
+            description = "Document attachment.",
+            example = "json, pdf, etc."
+    )
     DOCUMENT,
+
+    @Schema(
+            description = "Other attachment.",
+            example = "mp3, stl, etc."
+    )
     OTHER;
 }
--- src/main/java/com/travel/hero/currency/enumeration/CurrencyCode.java
+++ src/main/java/com/travel/hero/currency/enumeration/CurrencyCode.java
@@ -1,13 +1,21 @@
 package com.travel.hero.currency.enumeration;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Schema(description = "Currency code.")
 @RequiredArgsConstructor
 @Getter
 public enum CurrencyCode {
+
+    @Schema(description = "United States Dollar.")
     USD("United States Dollar"),
+
+    @Schema(description = "Euro.")
     EUR("Euro"),
+
+    @Schema(description = "Russian ruble.")
     RUB("Russian Ruble");
 
     private final String currencyFullName;
