980a1ef1f3e35255afb2a179289ebf94c48f8565
cemperlol <Baxeon96@yandex.ru>
2026-01-20 16:16:49 +0300

    added aspect logging for service layer '

---
+++ src/main/java/com/travel/hero/common/logging/LoggingAspect.java
@@ -0,0 +1,60 @@
+package com.travel.hero.common.logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Pointcut("execution(* com.travel.hero..service..*(..))")
+    public void serviceLayer() {}
+
+    @Around("serviceLayer()")
+    public Object logServiceMethods(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        long start = System.currentTimeMillis();
+
+        String className = joinPoint.getSignature().getDeclaringType().getSimpleName();
+        String methodName = joinPoint.getSignature().getName();
+
+        log.info("->️  {}.{}() called with args: {}",
+                className,
+                methodName,
+                Arrays.toString(joinPoint.getArgs())
+        );
+
+        try {
+            Object result = joinPoint.proceed();
+
+            long duration = System.currentTimeMillis() - start;
+
+            log.info("<-  {}.{}() finished in {} ms",
+                    className,
+                    methodName,
+                    duration
+            );
+
+            return result;
+
+        } catch (Throwable e) {
+            long duration = System.currentTimeMillis() - start;
+
+            log.error("<*> {}.{}() failed after {} ms",
+                    className,
+                    methodName,
+                    duration,
+                    e
+            );
+
+            throw e;
+        }
+    }
+}
