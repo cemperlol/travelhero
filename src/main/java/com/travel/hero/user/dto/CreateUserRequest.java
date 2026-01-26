a779494d3927afe4fa3e3905a3af0ef61e73e412
cemperlol <Baxeon96@yandex.ru>
2026-01-26 18:06:39 +0300

    documented CreateUserRequest

--- src/main/java/com/travel/hero/user/dto/CreateUserRequest.java
+++ src/main/java/com/travel/hero/user/dto/CreateUserRequest.java
@@ -1,7 +1,24 @@
 package com.travel.hero.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record CreateUserRequest(
+
+        @Schema(
+                description = "User email",
+                example = "mail@mail.com"
+        )
         String email,
+
+        @Schema(
+                description = "User unique username",
+                example = "SomePassword111"
+        )
         String password,
+
+        @Schema(
+                description = "User unique username",
+                example = "someusername111"
+        )
         String username
 ) { }
